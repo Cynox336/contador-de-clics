ee2d55490295b9c35e49fbb09c2bbccf36c92416
Cynox336 <cynox336@gmail.com>
2023-06-09 13:58:09 +0200

    Update 09/06/23 13:58

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import './App.css';
 import freeCodeCampLogo from "./Imagenes/FreeCodeCamp_logo.png";
+import Boton from "./Componentes/Boton";
+import Contador from "./Componentes/Contador";
+import {useState} from "react";
 
 function App() {
+
+  const [numClics, setNumClics] = useState(0);
+
+  const manejarClic = () => {
+    setNumClics(numClics + 1);
+  }
+
+  const reiniciarContador = () => {
+    setNumClics(0);
+  }
+
   return (
     <div className="App">
       <div className="freecodecamp-logo-contenedor">
@@ -10,7 +24,11 @@ function App() {
         alt="Logo de freeCodeCamp"/>
       </div>
       <div className="contenedor-principal">
-        
+        <Contador numClics= {numClics} />
+        <Boton texto="Clic" esBotonDeClic={true}
+        manejarClic={manejarClic}/>
+        <Boton texto="Reiniciar" esBotonDeClic={false}
+        manejarClic={reiniciarContador}/>
       </div>
     </div>
   );
